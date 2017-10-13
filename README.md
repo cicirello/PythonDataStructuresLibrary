ab4e3b9d4e91eeb07f6bda74fd00e9fb6ddd92b3
Vincent Cicirello <cicirello@users.noreply.github.com>
2017-10-13 08:50:51 -0400

    Added disjoint set forests

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Python Data Structures Library
-Library of data structures.  
+Library of data structures in Python.  
 
 Currently contains:
+* DisjointSets: Disjoint set forests with union by rank and path compression.
 * PQ: A binary heap implementation of a priority queue (with O(lg N) priority changes).
 
-Yes, that is it for now.  I will be adding much more relatively soon.  I wanted to get this one up here for students currently in CSIS 4104 (at Stockton University).
+Yes, that is it for now.  I will be adding much more relatively soon.  I wanted to get these up here for students currently in CSIS 4104 (at Stockton University).
---
+++ disjointsets.py
@@ -0,0 +1,82 @@
+## Python Data Structures Library
+##
+## Copyright (C) 2017 Vincent A. Cicirello.
+## http://www.cicirello.org/
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class DisjointSets :
+    """Disjoint Set Forests: Representation of disjoint sets of integers from the range [0..N-1].
+
+    Disjoint sets of integers from the range [0..size-1] are represented as
+    a disjoint set forest.  This implementation uses both the union by rank heuristic, as well as
+    path compression.
+    """
+
+
+    def __init__(self, size) :
+        """Initializes disjoint set forest of the integers in interval [0..size-1].
+
+        Each integer from 0 to size - 1 is initially in a set by itself.
+
+        Keyword arguments:
+        size -- number of elements in disjoint set forest.  The elements are integers from 0 to size-1.
+        """
+
+        # List of parent nodes, one for each element, in the tree representing
+        # a node's set. Root of tree has itself as parent.
+        self._p = [ x for x in range(size)]
+        # A node's rank is an upper bound on node height (given chain of parents to root).
+        self._rank = [ 0 for x in range(size)]
+
+
+    def union(self,x,y) :
+        """Union by rank.
+
+        Computes the union of the sets containing x and y.
+
+        Keyword arguments:
+        x -- an element, an integer in [0,size)
+        y -- an element, an integer in [0,size)
+        """
+        
+        self._link(self.findSet(x), self.findSet(y))
+
+
+    def findSet(self,x) :
+        """Finds the set id for a given element, and performs path compression.
+
+        Finds the set id for a given element, where the set id is the root of its
+        tree in the forest.  The find also performs path compression, resetting the parents
+        of all nodes along path to root to point directly to root.  Path compression does not
+        reset ranks, thus ranks are upper bounds only.
+
+        Keyword arguments:
+        x -- the element whose set we want to find
+        """
+        
+        if x != self._p[x] :
+            self._p[x] = self.findSet(self._p[x])
+        return self._p[x]
+
+
+        
+    def _link(self,x,y) :
+        if self._rank[x] > self._rank[y] :
+            self._p[y] = x
+        else :
+            self._p[x] = y
+            if self._rank[x] == self._rank[y] :
+                self._rank[y] = self._rank[y] + 1
